a312231ff342bd2a090a79f12a7f65ed7eea23fc
yavilar <yavilarobledo@ivytech.edu>
2023-11-06 14:15:34 -0500

    new animals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import './App.css';
+import { useState } from 'react';
+import AnimalShow from './AnimalShow';
+
+
+function getRandomAnimal(){
+    const animals=['bird','cat','cow','dog','gator','horse'];
+    return animals[Math.floor(Math.random()*animals.length)];
+
+}
+
 function App(){
+    const [animals,setAnimals]=useState([]);
+
+
     const handleClick=()=>{
-        console.log('Button was clicked!')
+        setAnimals([...animals,getRandomAnimal()]);
+       
     };
 
-    return <div>
+    const renderedAnimals=animals.map((animal,index)=>{
+        return<AnimalShow type={animal} key={index} />
+    });
+
+    return (
+    <div className="app">
         <button onClick={handleClick}>Add Animal</button>
+        <div className="animal-list">{renderedAnimals}</div>
     </div>
+    );
 }
 
 export default App;
